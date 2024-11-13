e278becf09f0c03c72aa447742d3fe2ab31d1705
jialiangteng <jialiangteng@creditease.cn>
2024-11-13 19:10:09 +0800

    feat:api logger filter

---
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/framework/aop/ApiLoggerFilter.java
@@ -0,0 +1,8 @@
+package cn.iocoder.yudao.module.system.framework.aop;/**
+ * @ClassName: ApiLoggerFilter
+ * @Description: TODO
+ * @Author: jialiangteng
+ * @Date: 2024/11/13 19:08
+**/
+public class ApiLoggerFilter {
+}
